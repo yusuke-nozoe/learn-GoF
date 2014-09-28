963ad985afc3f32cb1ca93e89272e7583d6552f7
Yusuke Nozoe <yusuke.nozoe@dena.jp>
2014-09-28 19:49:15 +0900

    add singleton.java

---
+++ java/Singleton.java
@@ -0,0 +1,10 @@
+final class Singleton {
+    private static Singleton instance;
+    private Singleton(){};
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
